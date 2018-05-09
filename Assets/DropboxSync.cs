341f50ef27d44a22a34c3625630138376bb5a787
George Fedoseev <george.fedoseev@me.com>
2018-05-09 22:15:57 +0300

    trying to get list of files

---
+++ Assets/DropboxSync.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Security;
+using System.Text;
+using UnityEngine;
+
+[Serializable]
+public class DropboxListFolderParams {
+	public string path;
+	public bool recursive = false;
+	public bool include_media_info = false;
+	public bool include_deleted = false;
+	public bool include_has_explicit_shared_members = false;
+	public bool include_mounted_folders = false;
+
+}
+
+public class DropboxSync : MonoBehaviour {
+
+	string accessToken = "sVawC-LSz14AAAAAAAAILN_nDwbybSkfaYkjXqJcoapoMI9NrFvh9iZoGb8jtkBq";
+
+	// Use this for initialization
+	void Start () {
+		TestDropbox();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	// METHODS
+
+	void TestDropbox(){
+		
+		InitiateSSLTrust();
+
+		using (var client = new WebClient()){
+			
+			var url = "https://api.dropboxapi.com/2/files/list_folder";
+			client.Headers.Set("Authorization", "Bearer "+accessToken);
+			client.Headers.Set("Content-Type", "application/json");				
+
+			var par = new DropboxListFolderParams{path="/"};
+
+			// var postData =	new System.Collections.Specialized.NameValueCollection(){
+			// 	{ "path", "/" }				
+			// };
+
+			var resp = client.UploadData(url, "POST", Encoding.Default.GetBytes(JsonUtility.ToJson(par)));
+
+			Debug.Log(resp);
+				
+			
+		}
+	}
+
+	public static void InitiateSSLTrust()
+	{
+		try
+		{
+			//Change SSL checks so that all checks pass
+			ServicePointManager.ServerCertificateValidationCallback =
+			new RemoteCertificateValidationCallback(
+					delegate
+					{ return true; }
+				);
+		}
+		catch (Exception ex)
+		{
+			Debug.LogException(ex);
+		}
+	}
+
+	// EVENTS
+}
