2d7a1c672f8d3cc41f9f6ac8d49c53a90e9cfc1b
George Fedoseev <georgii.fedoseev@giant.ai>
2022-12-02 20:05:27 +0300

    added enums

--- Assets/DropboxSync_v4/Model/RequestParameters/SharedLinkSettingsParameters.cs
+++ Assets/DropboxSync_v4/Model/RequestParameters/SharedLinkSettingsParameters.cs
@@ -1,9 +1,32 @@
 namespace DBXSync {
+
+    public struct LinkAudienceParam {
+        public const string PUBLIC = "public";
+        public const string TEAM = "team";
+        public const string NO_ONE = "no_one";
+
+    }
+
+    public struct RequestedLinkAccessLevelParam {
+        public const string VIEWER = "viewer";
+        public const string EDITOR = "editor";
+        public const string MAX = "max";
+        public const string DEFAULT = "default";
+
+    }
+
+    public struct RequestedVisibilityParam {
+        public const string PUBLIC = "public";
+        public const string TEAM_ONLY = "team_only";
+        public const string PASSWORD = "password";
+
+    }
+
     [System.Serializable]
     public class SharedLinkSettingsParameters : RequestParameters {
-        public string audience = "public";
-        public string access = "viewer";
-        public string requested_visibility = "public";
+        public string audience = LinkAudienceParam.PUBLIC;
+        public string access = RequestedLinkAccessLevelParam.VIEWER;
+        public string requested_visibility = RequestedVisibilityParam.PUBLIC;
         public bool allow_download = true;
     }
 }
